aea9032ec2b6eb8a1d93c7c8f0f5444c52dd8106
ethan <ejohnson16@myseneca.ca>
2020-04-24 22:28:04 -0400

    fix5

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ const credentials = require('./credentials');
 const express = require('express');
 const app = express();
 let server;
-let port;
-if (credentials.key && credentials.cert) {
-    const https = require('https');
-    server = https.createServer(credentials, app);
-    port = 8080;
-} else {
-    const http = require('http');
-    server = http.createServer(app);
-    port = 8080;
-}
+let port = process.env.PORT || 8080;
+
+  const http = require('http');
+  server = http.createServer(app);
+
 const io = require('socket.io')(server);
 const RoomService = require('./RoomService')(io);
 io.sockets.on('connection', RoomService.listen);
 io.sockets.on('error', e => console.log(e));
 app.use(express.static(__dirname + '/public'));
-app.get('*', function (req, res) {
+app.get('*', function(req, res) {
     res.sendFile(`${__dirname}/index.html`);
 });
 server.listen(port, () => console.log(`Server is running on port ${port}`));
